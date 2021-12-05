37cb169b182820caf26d2a72fb87888869371500
Nufayl Razick <nufayl.razick@ricardo.com>
2021-12-05 23:09:08 +0000

    feat: main game component

--- src/Game/Game.js
+++ src/Game/Game.js
@@ -1,12 +1,50 @@
-import React from 'react';
+import React from "react";
+import { Board, BoardOverlay } from "./board/";
+import Square from "./Square";
+import { PLAYER_1, PlayerSquare, PlayerPanel } from "./player";
+import useGame from "./useGame";
+
+const getDefaultGameState = () => {
+  return {
+    squares: new Array(9).fill(null),
+    active_player: PLAYER_1,
+  };
+};
 
 function Game() {
-    return (
-        <div>
-            Your game here
-        </div>
-    );
+  const { gameState, onSquareClick, resetGame, winningCombination, message } =
+    useGame(getDefaultGameState());
+  const { squares, active_player } = gameState;
+
+  const onResetGame = () => {
+    resetGame(getDefaultGameState());
+  };
+
+  return (
+    <>
+      <PlayerPanel active_player={active_player} onRest={onResetGame} />
+      <Board
+        winCombination={winningCombination}
+        boardOverlay={
+          message ? (
+            <BoardOverlay message={message} onRest={onResetGame} />
+          ) : null
+        }
+      >
+        {squares.map((player, index) => (
+          <Square
+            key={index}
+            id={index}
+            onClick={(squareId) => {
+              onSquareClick(squareId, active_player);
+            }}
+          >
+            {player ? <PlayerSquare player={player} /> : null}
+          </Square>
+        ))}
+      </Board>
+    </>
+  );
 }
 
-
-export default Game;
+export { Game, getDefaultGameState };
--- src/Game/Game.test.js
+++ src/Game/Game.test.js
@@ -1,9 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Game from './Game';
+import "@testing-library/jest-dom/extend-expect";
+import { cleanup, render, within, fireEvent } from "@testing-library/react";
+import { Game } from "./Game";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Game />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe("Game", () => {
+  afterEach(cleanup);
+  test("render game board with 9 squares with empty play", () => {
+    const { getByTestId } = render(<Game />);
+    const withinBoard = within(getByTestId("test-board-squares"));
+    const squaresBtn = withinBoard.getAllByRole("button");
+    expect(squaresBtn.length).toBe(9);
+    const playIcons = withinBoard.queryByTestId(/test-icon-x/i);
+    expect(playIcons).toBeNull();
+  });
+
+  test("squares illegal moves", () => {
+    const { getByTestId } = render(<Game />);
+    const squaresBtn = within(getByTestId("test-board-squares")).getAllByRole(
+      "button"
+    );
+    fireEvent.click(squaresBtn[0]);
+    expect(squaresBtn[0]).toHaveAttribute("disabled");
+  });
 });
